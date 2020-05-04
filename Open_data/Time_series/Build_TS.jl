1bed69c0bd689c0a4f859b9f3ac2175068012518
Gabriel A. Carranco-Sapiéns <32427033+carranco-sga@users.noreply.github.com>
2020-05-03 20:24:18 -0500

    TS: 2020-05-03.

--- Open_data/Time_series/Build_TS.jl
+++ Open_data/Time_series/Build_TS.jl
@@ -148,7 +148,7 @@ end
 #Cambiamos el directorio de trabajo a dónde se encuentran los datos.
 cd("Open_data/COVID-19")
 
-fecha_análisis = Date("2020-05-02")
+fecha_análisis = today()
 
 #Los datos se encuentran en formato .csv al interior del archivo .zip:
 datos_zip = ZipFile.Reader(Dates.format(fecha_análisis, "yyyymm")*"/"*Dates.format(fecha_análisis, "yyyymmdd")*".zip")
